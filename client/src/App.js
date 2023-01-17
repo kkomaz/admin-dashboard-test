a76ea01880b5eca73dae1d58f48dbcccdeb8bbbc
Alexander Lee <leeajl006@gmail.com>
2023-01-17 08:34:38 -0500

    Add browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/system/styles';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { themeSettings } from 'theme';
+
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            {/* Top route exist across all routes (Layout) */}
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
