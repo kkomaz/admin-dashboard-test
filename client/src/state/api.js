188e5cd57c878c5be00d99f60605476f72f21ac3
Alexander Lee <leeajl006@gmail.com>
2023-01-21 17:13:28 -0500

    Add customers

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: 'adminApi',
-  tagTypes: ['User', 'Product'],
+  tagTypes: ['User', 'Product', 'Customers'],
   endpoints: (build) => ({
     // useGetUserQuery
     getUser: build.query({
@@ -14,7 +14,12 @@ export const api = createApi({
       query: () => 'client/products',
       providesTags: ['Product'],
     }),
+    getCustomers: build.query({
+      query: () => 'client/customers',
+      providerTags: ['Customer'],
+    }),
   }),
 });
 
-export const { useGetUserQuery, useGetProductsQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery } =
+  api;
